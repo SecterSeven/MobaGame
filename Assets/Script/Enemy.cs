974d248fdb41f3a9581c6170eee02941b1e46389
Kantachai Rungruang <kantachai00_phot@hotmail.com>
2023-03-31 17:11:03 +0700

    Update Enemy.cs
    add

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    private int healthEnemy;
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthEnemy = 100;
     }
 
     // Update is called once per frame
